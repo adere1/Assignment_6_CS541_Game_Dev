6c3d26c45f3490bf0888c665f506baa1645781e4
Aditya Surendra Dere <adere1@users.noreply.github.com>
2019-04-23 10:16:32 -0400

    Connection Logic

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,52 +1,75 @@
 package com.example.myapplication;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    JSONParser jParser = new JSONParser();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button button = (Button) findViewById(R.id.button1);
+        StrictMode.setThreadPolicy(policy);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                String result = null;
+                InputStream is = null;
+                EditText editText = (EditText)findViewById(R.id.e1);
+                String v1 = editText.getText().toString();
+                EditText editText1 = (EditText)findViewById(R.id.e2);
+                EditText editText2 = (EditText)findViewById(R.id.e3);
+                String url = "http://149.125.136.144/selectdata.php";
+
+                URL currentUrl = null;
+                try {
+                    currentUrl = new URL(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                }
+                HttpURLConnection urlConnection = null;
+                InputStream in;
+                BufferedReader streamReader = null;
+                StringBuilder responseStrBuilder = new StringBuilder();
+                String inputStr;
+                try {
+                    urlConnection = (HttpURLConnection) currentUrl.openConnection();
+                    in = new BufferedInputStream(urlConnection.getInputStream());
+                    streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                    while ((inputStr = streamReader.readLine()) != null) {
+                        responseStrBuilder.append(inputStr);
+                    }
+                    //editText2.setText(responseStrBuilder.toString());
+                    JSONObject no = new JSONObject(responseStrBuilder.toString());
+                    JSONObject no1 = new JSONObject(no.getString("0"));
+                    editText2.setText(no1.getString("notedata"));
+                }catch(Exception e){
+                   Log.e("log_tag", "Error in http connection "+e.toString());
+                   Toast.makeText(getApplicationContext(), "Connection fail", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
