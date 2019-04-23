1d58035b7fc549e50417a12244d4cb9c6cd13a85
Aditya Surendra Dere <adere1@users.noreply.github.com>
2019-04-23 12:27:17 -0400

    Changes for Ui + JSON data

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,12 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import android.app.Activity;
@@ -55,16 +58,41 @@ public class MainActivity extends Activity
                 StringBuilder responseStrBuilder = new StringBuilder();
                 String inputStr;
                 try {
+
+                    String name = editText.getText().toString();
                     urlConnection = (HttpURLConnection) currentUrl.openConnection();
+                    /*String data = URLEncoder.encode("username", "UTF-8") + "="
+                            + URLEncoder.encode("Aditya", "UTF-8");;*/
+                    String data = URLEncoder.encode("name", "UTF-8")
+                            + "=" + URLEncoder.encode(name, "UTF-8");
+                    //urlConnection.setRequestMethod("POST");
+                    //urlConnection.setDoInput(true);
+                    urlConnection.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                    wr.write(data);
+                    wr.flush();
+                    //wr.close();
+                    Log.d(name,"pqr");
+
                     in = new BufferedInputStream(urlConnection.getInputStream());
                     streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
                     while ((inputStr = streamReader.readLine()) != null) {
                         responseStrBuilder.append(inputStr);
                     }
-                    //editText2.setText(responseStrBuilder.toString());
-                    JSONObject no = new JSONObject(responseStrBuilder.toString());
-                    JSONObject no1 = new JSONObject(no.getString("0"));
-                    editText2.setText(no1.getString("notedata"));
+                    //editText2.setText(responseStrBuilder.toString().split(name)[1]);
+                    try {
+                        int length = responseStrBuilder.toString().length();
+                        String abc = responseStrBuilder.toString().substring(name.length() + 1, length);
+                        JSONObject no = new JSONObject(abc);
+
+                        JSONObject no1 = new JSONObject(no.getString("0"));
+                        editText2.setText(no1.getString("notedata"));
+                        //Log.d(name,responseStrBuilder.toString());
+                    }catch (Exception e){
+                        editText2.setText("No Data Present");
+                    }
+
+
                 }catch(Exception e){
                    Log.e("log_tag", "Error in http connection "+e.toString());
                    Toast.makeText(getApplicationContext(), "Connection fail", Toast.LENGTH_SHORT).show();
